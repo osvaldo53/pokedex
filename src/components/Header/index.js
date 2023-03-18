11953df25f45b2873c1bfc52f0ac198fd2282535
Osvaldo Souza <osvaldosouza53@outlook.com>
2023-03-17 21:20:13 -0300

    att

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,14 +3,17 @@ import pokeball from './pokeball.png';
 
 function Header () {
    return(
-      <header className='header-box'>
-         <div className='div-logo'>
-            <h1 className='header-titulo'>Pokedex</h1>
-            <img src={pokeball} className='header-img' alt='pokeball'/>
-         </div>
-         
-         <button>X</button>
-      </header>
+      <>
+         <header className='header-box'>
+            <div className='div-logo'>
+               <h1 className='header-titulo'>Pokedex</h1>
+               <img src={pokeball} className='header-img' alt='pokeball'/>
+            </div>
+            
+            <button>X</button>
+         </header>
+         <div className='espaco-header'></div>
+      </>
    )
 }
 
