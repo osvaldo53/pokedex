0630bd3187bf67a86a597374bba7655fbf29b04b
Osvaldo Souza <osvaldosouza53@outlook.com>
2023-03-25 17:08:03 -0300

    alteraçoes

--- src/components/HamburgerMenu/index.js
+++ src/components/HamburgerMenu/index.js
@@ -15,19 +15,19 @@ function HamburguerMenu() {
    return(
       <>
          <span className="material-symbols-outlined button-icon menu-icon" 
-         onClick={handleClick} style={{ backgroundColor: isOpen ? "red" : "" }}>{conteudoBotao}</span>
+         onClick={handleClick} style={{ backgroundColor: isOpen ? "#EF4036" : "" }}>{conteudoBotao}</span>
          
          <div className={isOpen ? 'menu-open' : 'menu'}>
             <h1 className="h1-menu">Escolha a geração:</h1>
             <div className="geracoes-container">
-               <a href="/">1ª</a>
-               <a href="/">2ª</a>
-               <a href="/">3ª</a>
-               <a href="/">4ª</a>
-               <a href="/">5ª</a>
-               <a href="/">6ª</a>
-               <a href="/">7ª</a>
-               <a href="/">8ª</a>
+               <a href="/">1</a>
+               <a href="/">2</a>
+               <a href="/">3</a>
+               <a href="/">4</a>
+               <a href="/">5</a>
+               <a href="/">6</a>
+               <a href="/">7</a>
+               <a href="/">8</a>
             </div>
             <Footer/>
          </div>
--- src/pages/Pokemon/index.js
+++ src/pages/Pokemon/index.js
@@ -25,14 +25,32 @@ function Pokemon() {
       return (str.charAt(0).toUpperCase() + str.substr(1))
    }
 
+   const types = () => {
+      if (pokemon.types[1]) {
+         return (pokemon.types ? pokemon.types[0].type.name : '') 
+         + ' / '  + (pokemon.types ? pokemon.types[1].type.name : '')
+      }
+      if (pokemon.types[0]) {
+         return (pokemon.types ? pokemon.types[0].type.name : '')
+      }
+   }
+
+   console.log(pokemon);
    
    return(
       <div className="pokemon-info">
-         <h1>
+
+         <h1 className="pokemon-h1">
             #{pokemon.id ? pokemon.id.toString().padStart(3, '0') : ''} {pokemon.name ? capitalize(pokemon.name) : ''}
          </h1>
+         
+         <img src={pokemon.sprites ? pokemon.sprites.other["official-artwork"].front_default : ''} alt={pokemon.name}></img>
 
-         <img src={pokemon.sprites ? pokemon.sprites.front_default : ''} alt={pokemon.name}></img>
+         <p className="pokemon-types-p">{pokemon.types ? types() : ''}</p>
+
+         <p className="altura-peso-p">Altura: {pokemon.height ? pokemon.height/10 : ''} m</p>
+         <p className="altura-peso-p">Peso: {pokemon.weight ? pokemon.weight/10 : ''} Kg</p>
+         
          
 
       </div>
